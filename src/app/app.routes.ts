dc74e3a1fd18f7512fb004cfa22f552ab479a5d7
Petar Gutov <pgutov@gmail.com>
2024-05-13 15:56:32 +0200

    add home component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 export const routes: Routes = [
+    { path: '', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent) },
     { path: '404', component: PageNotFoundComponent },
     { path: '**', redirectTo: '/404' }
 ];
