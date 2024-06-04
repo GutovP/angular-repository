dd08cdd4eeaed763905061ee8bf269559e3b1f84
Petar Gutov <pgutov@gmail.com>
2024-06-04 15:57:55 +0200

    creating typewriter component

---
+++ src/app/core/typewriter/typewriter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typewriter',
+  standalone: true,
+  imports: [],
+  templateUrl: './typewriter.component.html',
+  styleUrl: './typewriter.component.scss'
+})
+export class TypewriterComponent {
+
+}
