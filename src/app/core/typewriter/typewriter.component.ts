5498e0f5c051282e8eba069cb251627777dd4603
Petar Gutov <pgutov@gmail.com>
2025-01-14 18:11:03 +0100

    styles added

--- src/app/core/typewriter/typewriter.component.ts
+++ src/app/core/typewriter/typewriter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-typewriter',
@@ -9,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class TypewriterComponent {
 
+  private i: number = 0;
+  public text: string = 'Lorem ipsum dummy text blabla.';
+  private speed: number = 50;
+
+  typeWriter() {
+
+    if (this.i < this.text.length) {
+
+      document.querySelector('.demo')!.innerHTML += this.text[this.i];
+      this.i++;
+      setTimeout(() => this.typeWriter(), this.speed);
+    }
+
+  }
 }
